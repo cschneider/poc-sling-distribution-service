e20380e6c50071f456bf1cffc403972a6204685b
Christian Schneider <cschneid@adobe.com>
2020-04-02 20:24:36 +0200

    Adding felix systemready

--- backend/src/main/java/org/apache/sling/distribution/service/impl/package-info.java
+++ backend/src/main/java/org/apache/sling/distribution/service/impl/package-info.java
@@ -1,3 +1,6 @@
+@RequireConfigurator
 @org.osgi.annotation.versioning.Version("0.1.0")
 @org.osgi.annotation.bundle.Export
 package org.apache.sling.distribution.service.impl;
+
+import org.osgi.service.configurator.annotations.RequireConfigurator;
